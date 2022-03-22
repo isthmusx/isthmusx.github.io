bedeb2d08ceba10605803ebf178ca7382768ac81
John Paul Al-haddi <70573351+isthmusx@users.noreply.github.com>
2022-03-22 16:39:40 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -31,3 +31,19 @@ anime.timeline({loop: false})
    // delay: 1000
    
   });
+  
+
+// Get the container element
+var btnContainer = document.getElementById("navActive");
+
+// Get all buttons with class="btn" inside the container
+var btns = btnContainer.getElementsByClassName("btn");
+
+// Loop through the buttons and add the active class to the current/clicked button
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
