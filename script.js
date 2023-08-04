eb4c695c5888757bf939f29d34d702b779d7cc26
John Paul Al-haddi <jpaul.alhaddi@gmail.com>
2023-08-05 03:41:19 +0800

    idunno

--- script.js
+++ script.js
@@ -36,7 +36,7 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         const target = document.querySelector(this.getAttribute('href'));
         if (target) {
             window.scrollTo({
-                top: target.offsetTop - 100, // Adjust this offset based on your navigation bar height
+                top: target.offsetTop - 0, // Adjust this offset based on your navigation bar height
                 behavior: 'smooth'
             });
         }
